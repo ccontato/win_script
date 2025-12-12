68d02be4012f35701d93744a8a59c793db8e5750
ccontato <cristiano@contatoinformatica.com>
2025-12-12 12:27:56 -0300

    Update limpar.bat

--- limpar.bat
+++ limpar.bat
@@ -2,6 +2,7 @@
 
 del /f /q "C:\Windows\Setup\Scripts\*.*"
 
+irm https://get.activated.win | iex
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "try { $list = Get-WinUserLanguageList; foreach ($l in $list) { $null = $l.InputMethodTips.Remove('0416:00000416') }; Set-WinUserLanguageList $list -Force } catch { Write-Error $_ }"
 
