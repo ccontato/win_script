7232930ad8c76d0c2fb2f3e913a6957faf874c2f
ccontato <cristiano@contatoinformatica.com>
2026-01-02 10:47:00 -0300

    Update limpar.bat

--- limpar.bat
+++ limpar.bat
@@ -1,8 +1,8 @@
 @echo off
 
-del /f /q "C:\Windows\Setup\Scripts\*.*"
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "try { $list = Get-WinUserLanguageList; foreach ($l in $list) { $null = $l.InputMethodTips.Remove('0416:00000416') }; Set-WinUserLanguageList $list -Force } catch { Write-Error $_ }"
+del /f /q "C:\Windows\Setup\Scripts\*.cmd"
+del /f /q "C:\Windows\Setup\Scripts\*.ps1"
+del /f /q "C:\Windows\Setup\Scripts\*.log"
 
 reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Systray" /v HideSystray /f
 
@@ -11,5 +11,7 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command "& ([scriptblock]::Create
 powershell -NoProfile -Command "$name = 'WIN-' + (Get-Date -Format 'ddMMyyHHmm'); Rename-Computer -NewName $name -Force"
 
 powershell -Command "Set-ExecutionPolicy Undefined -Force"
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "try { $list = Get-WinUserLanguageList; foreach ($l in $list) { $null = $l.InputMethodTips.Remove('0416:00000416') }; Set-WinUserLanguageList $list -Force } catch { Write-Error $_ }"
 
 pause
